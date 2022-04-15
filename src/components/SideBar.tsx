7085f520afc94b396be9cbcded9f8fbcfbd78fc0
Stuart Green <github.47a8z@aleeas.com>
2022-04-15 14:11:32 +0100

    feat: add sort by price

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,44 +1,110 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { useLocation, NavLink } from "react-router-dom";
+
+import qs from "qs";
 
 import "../styles/SideBar.css";
 
+interface QueryCity {
+  city: string;
+}
+
+interface QuerySort {
+  price: number;
+}
+
+type ValueObj = QueryCity | QuerySort;
+
 const SideBar: React.FC = (): JSX.Element => {
+  const buildQueryString = (operation: string, valueObj: ValueObj): string => {
+    const { search } = useLocation();
+
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify(valueObj),
+    };
+
+    return qs.stringify(newQueryParams, {
+      addQueryPrefix: true,
+      encode: false,
+    });
+  };
+
   return (
     <aside className="sidebar">
       <section className="sidebar__city">
+        <h3 className="sidebar__header">Filter by City</h3>
         <ul className="sidebar__list">
-          <NavLink
-            exact
-            to={`/?query={"city": "Manchester"}`}
-            className="sidebar__item"
-          >
-            Manchester
-          </NavLink>
+          <li className="sidebar__list-item">
+            <NavLink
+              exact
+              to={buildQueryString("query", { city: "Manchester" })}
+              className="sidebar-link"
+              activeClassName="selected"
+            >
+              Manchester
+            </NavLink>
+          </li>
 
-          <NavLink
-            exact
-            to={`/?query={"city": "Liverpool"}`}
-            className="sidebar__item"
-          >
-            Liverpool
-          </NavLink>
+          <li className="sidebar__list-item">
+            <NavLink
+              exact
+              to={buildQueryString("query", { city: "Liverpool" })}
+              className="sidebar-link"
+              activeClassName="selected"
+            >
+              Liverpool
+            </NavLink>
+          </li>
 
-          <NavLink
-            exact
-            to={`/?query={"city": "London"}`}
-            className="sidebar__item"
-          >
-            London
-          </NavLink>
+          <li className="sidebar__list-item">
+            <NavLink
+              exact
+              to={buildQueryString("query", { city: "London" })}
+              className="sidebar-link"
+              activeClassName="selected"
+            >
+              London
+            </NavLink>
+          </li>
 
-          <NavLink
-            exact
-            to={`/?query={"city": "Bristol"}`}
-            className="sidebar__item"
-          >
-            Bristol
-          </NavLink>
+          <li className="sidebar__list-item">
+            <NavLink
+              exact
+              to={buildQueryString("query", { city: "Bristol" })}
+              className="sidebar-link"
+              activeClassName="selected"
+            >
+              Bristol
+            </NavLink>
+          </li>
+        </ul>
+
+        <h3 className="sidebar__header">Sort by</h3>
+        <ul className="sidebar__list">
+          <li className="sidebar__list-item">
+            <NavLink
+              exact
+              to={buildQueryString("sort", { price: 1 })}
+              className="sidebar-link"
+              activeClassName="selected"
+            >
+              Price Ascending
+            </NavLink>
+          </li>
+
+          <li className="sidebar__list-item">
+            <NavLink
+              exact
+              to={buildQueryString("sort", { price: -1 })}
+              className="sidebar-link"
+              activeClassName="selected"
+            >
+              Price Descending
+            </NavLink>
+          </li>
         </ul>
       </section>
     </aside>
